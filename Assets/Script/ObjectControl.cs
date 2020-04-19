e6aaf91566e34d664e0fbb7579f35876d7e05882
Shilpa Roy <shilparoy4899@gmail.com>
2020-04-19 18:57:55 -0400

    initial commit, 04/19

---
+++ Assets/Script/ObjectControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectControl : MonoBehaviour
+{
+    private bool holding;
+    public Camera c;
+    public GameObject frame;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        holding = false;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (holding)
+        {
+            transform.position = c.transform.position + c.transform.forward;
+            transform.forward = c.transform.forward * -1;
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                if (Vector3.Distance(transform.position, frame.transform.position) < 2)
+                {
+                    transform.position = frame.transform.position;
+                    rb.constraints = RigidbodyConstraints.FreezePosition;
+
+                    holding = false;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                holding = false;
+                rb.constraints = RigidbodyConstraints.None;
+                rb.constraints = RigidbodyConstraints.FreezeRotation;
+            }
+        }
+    }
+
+    void OnMouseDown()
+    {
+        holding = true;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # GetOut
-A classic escape room type game built using the Unity Engine and presented as a final project for the class CMSC425
+This is a classic escape room type game built using the Unity Engine and presented as a final project for the class CMSC425.
+
+
+Welcome to shutter island! You are Detective Teddy Daniels, here to solve the mystery of the missing doctor
+Rachel Solando. Wade through the labyrinth of Shutter Island to see if you can uncover the truth.
+
+It will feature having to navigate through several distinct rooms, each with their own puzzles, under an exhausting time limit.
+*No copyright infringement intended. This is just a game we made for fun (and also for a grade)*
