97232b3dbac39a02a20e25ab62281e1bdd7012c9
Aaron Alem <aalem78@Aarons-MBP.home>
2020-04-23 17:13:12 -0400

    three room walls

---
+++ Assets/Script/Room2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room2 : MonoBehaviour
+{
+    public GameObject door;
+    private Animator doorAm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorAm = door.GetComponent<Animator>();
+        doorAm.SetBool("open", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
