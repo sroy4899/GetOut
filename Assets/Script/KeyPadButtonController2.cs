a8f4521822550a13c5a90d525b101a6ed4a548fc
Shilpa Roy <shilparoy4899@gmail.com>
2020-04-28 23:04:54 -0400

    4/28

---
+++ Assets/Script/KeyPadButtonController2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyPadButtonController2 : MonoBehaviour
+{
+    void OnMouseDown() {
+        if(!KeyPad2.opened) { 
+            if(name == "Clr") { 
+                KeyPad2.entry = "";
+            } 
+            else if(name == "Enter") { 
+                KeyPad2.check = true;
+            } 
+            else if(KeyPad2.entry.Length < 4) { 
+                KeyPad2.entry += name;
+            }
+        }
+    }
+}
