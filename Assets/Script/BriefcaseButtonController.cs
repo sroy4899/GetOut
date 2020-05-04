848d50b1116f514de21678afe5447f8ef6fe7d65
Aaron Alem <aalem78@Aarons-MBP.home>
2020-05-03 22:05:22 -0400

    briefcase doneish

---
+++ Assets/Script/BriefcaseButtonController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BriefcaseButtonController : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        if(name == "1Up")
+        {
+            int textVal = int.Parse(BriefcaseController.text1);
+            BriefcaseController.text1 = "" + nextVal(textVal, true);
+        }
+
+        if (name == "2Up")
+        {
+            int textVal = int.Parse(BriefcaseController.text2);
+            BriefcaseController.text2 = "" + nextVal(textVal, true);
+        }
+
+        if (name == "3Up")
+        {
+            int textVal = int.Parse(BriefcaseController.text3);
+            BriefcaseController.text3 = "" + nextVal(textVal, true);
+        }
+
+        if (name == "4Up")
+        {
+            int textVal = int.Parse(BriefcaseController.text4);
+            BriefcaseController.text4 = "" + nextVal(textVal, true);
+        }
+
+        if (name == "5Up")
+        {
+            int textVal = int.Parse(BriefcaseController.text5);
+            BriefcaseController.text5 = "" + nextVal(textVal, true);
+        }
+
+        if (name == "6Up")
+        {
+            int textVal = int.Parse(BriefcaseController.text6);
+            BriefcaseController.text6 = "" + nextVal(textVal, true);
+        }
+
+        if (name == "1Down")
+        {
+            int textVal = int.Parse(BriefcaseController.text1);
+            BriefcaseController.text1 = "" + nextVal(textVal, false);
+        }
+
+        if (name == "2Down")
+        {
+            int textVal = int.Parse(BriefcaseController.text2);
+            BriefcaseController.text2 = "" + nextVal(textVal, false);
+        }
+
+        if (name == "3Down")
+        {
+            int textVal = int.Parse(BriefcaseController.text3);
+            BriefcaseController.text3 = "" + nextVal(textVal, false);
+        }
+
+        if (name == "4Down")
+        {
+            int textVal = int.Parse(BriefcaseController.text4);
+            BriefcaseController.text4 = "" + nextVal(textVal, false);
+        }
+
+        if (name == "5Down")
+        {
+            int textVal = int.Parse(BriefcaseController.text5);
+            BriefcaseController.text5 = "" + nextVal(textVal, false);
+        }
+
+        if (name == "6Down")
+        {
+            int textVal = int.Parse(BriefcaseController.text6);
+            BriefcaseController.text6 = "" + nextVal(textVal, false);
+        }
+    }
+
+    int nextVal(int curr, bool up)
+    {
+        if (up)
+        {
+            return (curr + 1) % 10;
+        } else
+        {
+            int newVal = curr - 1;
+
+            if(newVal < 0)
+            {
+                return newVal + 10;
+            } else
+            {
+                return newVal;
+            }
+        }
+    }
+}
---
+++ Assets/Script/BriefcaseController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BriefcaseController : MonoBehaviour
+{
+    public Text box1;
+    public Text box2;
+    public Text box3;
+    public Text box4;
+    public Text box5;
+    public Text box6;
+
+    public static string text1 = "0";
+    public static string text2 = "0";
+    public static string text3 = "0";
+    public static string text4 = "0";
+    public static string text5 = "0";
+    public static string text6 = "0";
+
+    // Update is called once per frame
+    void Update()
+    {
+        box1.text = text1;
+        box2.text = text2;
+        box3.text = text3;
+        box4.text = text4;
+        box5.text = text5;
+        box6.text = text6;
+    }
+}
